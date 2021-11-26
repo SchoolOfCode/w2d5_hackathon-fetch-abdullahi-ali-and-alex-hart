88ab28de53abdb1450cf1c2dbbd05c82bb1cbaef
Abdullahi Ali <ali259abdullahi@gmail.com>
2021-11-26 15:53:12 +0000

    js fetch working

--- main.js
+++ main.js
@@ -3,36 +3,44 @@ async function fetchKantoPokemon(){
     let response = await thePromis.json();
     let allpokemon = response.results
     console.log(allpokemon)
-    //allpokemon.forEach(addToDropDown)
- }  
- 
+    
+    allpokemon.forEach(addToDropDown)
+
+}  
+let dataList = document.querySelector("datalist")
  function addToDropDown(pokemon){
+     
      let name = pokemon.name;
      let newPokemonToOptions = document.createElement("option")
+     //console.log(newPokemonToOptions)
      newPokemonToOptions.value = name;
-     
-     
-     //thedropdownvariable.innerchild = newPokemonToOptions
+     dataList.innerchild = newPokemonToOptions
+     //console.log(dataList)
+
  }
  
  fetchKantoPokemon()
  //function called when submit button is pressed with ${selectedPokemon}
- let hp = document.getElementById("");
- let attack = document.getElementById("");
- let defense = document.getElementById("") ;
- let speed = document.getElementById("");
- let specialAttack = document.getElementById("");
- let specialDefense = document.getElementById("");
- let total = document.getElementById("");
- let chosenPokemon = document.querySelector("input").innerText
+ let hp = document.getElementById("hp");
+ let attack = document.getElementById("attack");
+ let defense = document.getElementById("defense") ;
+ let speed = document.getElementById("speed");
+ let specialAttack = document.getElementById("specialAttack");
+ let specialDefense = document.getElementById("specialDefense");
+ let total = document.getElementById("total");
  
+ let submit = document.querySelector("button");
+ 
+ submit.addEventListener("click", fetchPokemonStats)
  
  async function fetchPokemonStats(){
+     let chosenPokemon = document.querySelector("input").value
 
- 
-     let thePromis = await fetch(`https://pokeapi.co/api/v2/pokemon/`)
+     console.log(chosenPokemon)
+     let thePromis = await fetch(`https://pokeapi.co/api/v2/pokemon/${chosenPokemon}`)
      let theResponse = await thePromis.json();
+     console.log(theResponse)
      let theStats = theResponse.stats;
      console.log(theStats);
  }
- fetchPokemonStats()
+ 
